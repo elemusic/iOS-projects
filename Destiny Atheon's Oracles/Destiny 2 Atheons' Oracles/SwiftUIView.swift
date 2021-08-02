f57cd175a0305ff51cefba5919e2295a44bfefce
elemusic <m.debski@me.com>
2021-08-02 21:34:25 +0200

    Add files via upload

---
+++ Destiny Atheon's Oracles/Destiny 2 Atheons' Oracles/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Xylophone
+//
+//  Created by Mateusz Dębski on 28/05/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
